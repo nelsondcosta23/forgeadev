d8b20c1281278931eba9231106ca9b60f50dac88
Nelson Costa <nelsonfilipecosta@gmail.com>
2026-04-13 18:03:37 +0100

    Fix: Express 5 compatibility for catch-all route

--- server.js
+++ server.js
@@ -340,7 +340,7 @@ app.get('/build/:sessionId', async (req, res) => {
   }
 });
 
-app.get('*', (req, res) => res.sendFile(path.join(distPath, 'index.html')));
+app.get('*splat', (req, res) => res.sendFile(path.join(distPath, 'index.html')));
 
 const server = app.listen(PORT, () => console.log(`BFF listening on ${PORT}`));
 
